7b16dff287a599e84ba1ad9022f09705e286b413
ttknpde-v <thitikorn_nupan@outlook.co.th>
2024-05-12 10:33:25 +0700

    commit

--- src/main/java/com/ttknpdev/myportfolio/controller/ControlPath.java
+++ src/main/java/com/ttknpdev/myportfolio/controller/ControlPath.java
@@ -2,6 +2,7 @@ package com.ttknpdev.myportfolio.controller;
 
 import com.ttknpdev.myportfolio.repository.LineNotifyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,10 +17,15 @@ public class ControlPath {
         this.lineNotifyRepo = lineNotifyRepo;
     }
 
+    @GetMapping(value = "/server")
+    private ResponseEntity<String> testServer() {
+        return ResponseEntity.ok("ok");
+    }
+
     @GetMapping(value = "/")
     private String start() throws Exception {
         lineNotifyRepo.sendLineNotifyMessage();
-        TimeUnit.MILLISECONDS.sleep(2000);
+        TimeUnit.MILLISECONDS.sleep(2000); // delay 2s then response index html
         return "index";
     }
 }
